cc0766822f363a2f84f3c613bfe61746b6930baa
Michael Legleux <mlegleux@ripple.com>
2022-03-03 20:51:05 -0800

    oh where is it then

--- build_clio.sh
+++ build_clio.sh
@@ -2,5 +2,7 @@
 
 git clone https://github.com/legleux/clio.git clio_src
 pushd clio_src && git checkout clio_deb_package && popd
+
+ls -l
 cmake -S /clio_src/ -B  /clio_src/build/
 cmake --build /clio_src/build --parallel $(nproc)
