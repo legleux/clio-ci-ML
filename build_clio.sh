095db95da9e12d91ba43e9feef1148bd933216d9
Michael Legleux <mlegleux@ripple.com>
2022-03-03 22:31:57 -0800

    one step back

--- build_clio.sh
+++ build_clio.sh
@@ -3,5 +3,4 @@
 git clone https://github.com/legleux/clio.git clio_src
 pushd clio_src && git checkout clio_deb_package
 cmake -B build
-cmake --build build --parallel 1
-3,978,656
+cmake --build build --parallel 2
