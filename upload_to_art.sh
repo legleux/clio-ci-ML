4dd2ad9b66b642a4c9c06d766948420b63ad4d93
Michael Legleux <mlegleux@ripple.com>
2022-03-21 18:54:50 -0700

    remove testing code; really fail

--- upload_to_art.sh
+++ upload_to_art.sh
@@ -1,21 +1,16 @@
+set -e
 RIPPLE_URL="https://artifactory.ops.ripple.com:443/artifactory"
 RIPPLE_CREDS="-urippled:${ARTIFACTORY_DEPLOY_KEY_RIPPLED}"
-TEST_URL="https://legleux.jfrog.io/artifactory"
-TEST_CREDS="-ulegleux@gmail.com:${LEGLEUX_ART_PW}"
-
 up_dpkg(){
   TARGET_FILE_PATH=$(realpath $1)
   PKG="$(basename $1)"
   ARTIFACTORY_URL=$RIPPLE_URL
-  # ARTIFACTORY_URL=$TEST_URL
-  LTEST_REPO="clio-debian"
   TEST_REPO="clio-deb-test-mirror"
   REAL_REPO="clio-deb"
   REPO=$TEST_REPO
   COMPONENT="beta"
   ARCHITECTURE="amd64"
   DISTROS="focal" # hirsute impish jammy"
-  # CREDS=$TEST_CREDS
   CREDS=$RIPPLE_CREDS
   for dist in $DISTROS ; do
       if [ -z "$DISTROS_MATRIX" ]; then DISTROS_MATRIX=";";fi
@@ -28,8 +23,6 @@ up_dpkg(){
       $CREDS \
       -XPUT "$ART_URI" \
       -T $TARGET_FILE_PATH
-  echo "Exit in func $?"
 }
 
 up_dpkg $1
-echo "Func out exit $?"
