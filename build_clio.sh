9d0279790c5c98b55719ffe1584b0d853835d92c
Michael Legleux <mlegleux@ripple.com>
2022-03-21 21:33:48 -0700

    stop pkg step

--- build_clio.sh
+++ build_clio.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
 git clone https://github.com/legleux/clio.git clio_src
-pushd clio_src && git checkout clio_deb_package
-cmake -B build
-cmake --build build --parallel 2
+pushd clio_src
+git checkout pkg-in-other-repo
+cp -r ../CMake/packaging CMake/
+cmake -B build -DPACKAGE=1
+cmake --build build --parallel $(nproc)
 cd build && cpack -G DEB
